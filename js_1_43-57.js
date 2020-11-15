d9069a7f8fbde32eacb13b8c3d82518922804516
Engramar Bollas <engramar.bollas@gmail.com>
2020-11-15 20:50:22 +1100

    Create js_1_43-57.js

---
+++ js_1_43-57.js
@@ -0,0 +1,55 @@
+#Manipulate Arrays With pop()
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+var removedFromMyArray = myArray.pop();
+
+#Manipulate Arrays With shift()
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+var removedFromMyArray = myArray.shift();
+
+#Manipulate Arrays With unshift()
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul",35]);
+
+#Shopping List
+var myList = [
+    ["Chocolate 1", 15],
+    ["Chocolate 2", 15],
+    ["Chocolate 3", 15],
+    ["Chocolate 4", 15],
+    ["Chocolate 5", 15]
+];
+
+#Write Reusable JavaScript with Functions
+function reusableFunction() {
+  console.log("Hi World");
+}
+reusableFunction();
+
+#Passing Values to Functions with Arguments
+function functionWithArgs(param1, param2) {
+  console.log(param1+param2);
+}
+functionWithArgs(1,2);
+
+#Global Scope and Functions
+
+
+
+#Local Scope and Functions
+#Global vs. Local Scope in Functions
+#Return a Value from a Function with Return
+#Understanding Undefined Value returned from a Function
+#Assignment with a Returned Value
+#Stand in Line
+#Understanding Boolean Values
+#Use Conditional Logic with If Statements
