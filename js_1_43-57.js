997db9b88a69a53d212048c9e7adc61a1967753b
Engramar Bollas <engramar.bollas@gmail.com>
2020-11-15 21:29:37 +1100

    Update js_1_43-57.js

--- js_1_43-57.js
+++ js_1_43-57.js
@@ -42,14 +42,120 @@ function functionWithArgs(param1, param2) {
 functionWithArgs(1,2);
 
 #Global Scope and Functions
+// Declare the myGlobal variable below this line
+var myGlobal = 10;
 
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
 
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
 
 #Local Scope and Functions
+function myLocalScope() {
+  'use strict';
+
+  // Only change code below this line
+  var myVar;
+  console.log('inside myLocalScope', myVar);
+}
+
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
+
+
 #Global vs. Local Scope in Functions
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+
 #Return a Value from a Function with Return
+function timesFive(num) {
+  return num * 5;
+}
+var answer = timesFive(5); 
+
 #Understanding Undefined Value returned from a Function
+// Setup
+var sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+}
+
+// Only change code above this line
+addThree();
+addFive();
+
 #Assignment with a Returned Value
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
+
 #Stand in Line
+function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item);
+  return arr.shift();
+  // return item;
+  // Only change code above this line
+}
+
+// Setup
+var testArr = [1,2,3,4,5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
 #Understanding Boolean Values
+function welcomeToBooleans() {
+  // Only change code below this line
+  return true; // Change this line
+  // Only change code above this line
+}
+
 #Use Conditional Logic with If Statements
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+  if (wasThatTrue){
+    return "Yes, that was true"
+  }
+  return "No, that was false"
+  // Only change code above this line
+}
+
